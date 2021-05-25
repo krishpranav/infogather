d388b8f40df0ec984e3cc86f54076e1fa7929d63
krishpranav <krisna.pranav@gmail.com>
2021-05-25 11:23:02 +0530

    [+] README updated [+]

--- pkg/authfree/melissa/melissa_test.go
+++ pkg/authfree/melissa/melissa_test.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/maraudery/krishpranav/infogather/config"
-	"github.com/maraudery/krishpranav/infogather/http"
+	"github.com/krishpranav/infogather/config"
+	"github.com/krishpranav/infogather/http"
 )
 
 func TestEmailLookup(t *testing.T) {
