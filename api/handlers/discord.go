976c9e2f0aa901bbe71809e83d5c9ad63e4b4de0
krishpranav <krisna.pranav@gmail.com>
2021-05-25 13:07:55 +0530

    [+] discord token info grabber [+]

--- api/handlers/discord.go
+++ api/handlers/discord.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/krishpranav/infogather/pkg/noauth/discord"
 )
 
 type DiscordHandler struct {
@@ -9,4 +12,15 @@ type DiscordHandler struct {
 
 func (h DiscordHandler) Index(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusNotFound).SendFile("./views/pages/discord.html")
+}
+
+func (h DiscordHandler) Show(ctx *fiber.Ctx) error {
+	token := ctx.Params("token")
+
+	tokeninfo, err := discord.TokenLookup(token)
+	if err != nil {
+		return errors.New("error")
+	}
+
+	return ctx.JSON(tokeninfo)
 }
