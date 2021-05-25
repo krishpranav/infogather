74033866b432e870d72c46b8b71256dce4f61289
krishpranav <krisna.pranav@gmail.com>
2021-05-25 12:59:17 +0530

    [+] user lookup function [+]

--- pkg/noauth/userlookup/userlookup.go
+++ pkg/noauth/userlookup/userlookup.go
@@ -1,9 +1,28 @@
 package userlookup
 
+import "sync"
+
 type Website struct {
 	Title  string
 	Domain string
 	Delete string
 	Valid  bool
 	Extra  string
+}
+
+var results []Website
+
+func UserLookup(userres string) []Website {
+	var wg sync.WaitGroup
+	var arrNo []Website = noRedirSites(userres)
+	for _, v := range arrNo {
+		go checkUser(v, userres, false, &wg)
+	}
+	var arrYes []Website = redirSites(userres)
+	for _, v := range arrYes {
+		go checkUser(v, userres, true, &wg)
+	}
+
+	wg.Wait()
+	return results
 }
