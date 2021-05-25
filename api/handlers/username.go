71e760d2ba4b56cae42ad46dd628adca7d21d718
krishpranav <krisna.pranav@gmail.com>
2021-05-25 13:17:48 +0530

    [+] username handler [+]

--- api/handlers/username.go
+++ api/handlers/username.go
@@ -1,10 +1,25 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/krishpranav/infogather/pkg/noauth/userlookup"
+)
 
+// UsernameHandler is ...
 type UsernameHandler struct {
 }
 
-func (h UsernameHandler) Index(ctx *fiber.Ctx) {
+// Index ...
+func (h UsernameHandler) Index(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusNotFound).SendFile("./views/pages/username.html")
+}
+
+// Show user information
+func (h UsernameHandler) Show(ctx *fiber.Ctx) error {
+	username := ctx.Params("username")
+
+	userinfo := userlookup.UserLookup(username)
+
+	// return ctx.JSON(fiber.Map{"data": userinfo})
+	return ctx.JSON(userinfo)
 }
