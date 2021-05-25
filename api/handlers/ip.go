a5934ef57d0373d7a466821630693e1c1e14d5f4
krishpranav <krisna.pranav@gmail.com>
2021-05-25 13:12:34 +0530

    [+] ip lookup handler [+]

--- api/handlers/ip.go
+++ api/handlers/ip.go
@@ -1,10 +1,24 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	hits "github.com/krishpranav/infogather/pkg/noauth/ip"
+)
 
 type IpHandler struct {
 }
 
 func (h IpHandler) Index(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusNotFound).SendFile("./views/pages/ip.html")
+}
+
+func (h IpHandler) Show(ctx *fiber.Ctx) error {
+	ip := ctx.Params("ip")
+
+	ipinfo, err := hits.IPLookup(ip)
+	if err != nil {
+		return ctx.JSON(fiber.Map{"message": "failure"})
+	}
+
+	return ctx.JSON(ipinfo)
 }
