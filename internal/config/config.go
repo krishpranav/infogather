6699692bc4a60cfb3bb879e9b7e6c7f586708719
krishpranav <krisna.pranav@gmail.com>
2021-05-25 11:55:13 +0530

    [+] config, config test update [+]

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,31 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
 type Conf struct {
 	MelissaKey string `json:"melissaKeyCred"`
 	HibpKey    string `json:"hibpKey"`
 	DataGovKey string `json:"dataGovKey"`
+}
+
+func LoadConfig(path string) (*Conf, error) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	conf := new(Conf)
+	err = json.Unmarshal(byteValue, &conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
 }
