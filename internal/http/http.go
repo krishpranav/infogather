0a4741f60906fa5d961fcd2747220300d2da50b4
krishpranav <krisna.pranav@gmail.com>
2021-05-25 11:34:47 +0530

    [+] update http functions [+]

---
+++ internal/http/http.go
@@ -0,0 +1,44 @@
+package https
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func GetReq(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return (string(body)), nil
+}
+
+func AuthGet(url string, authkey string, authval string) ([]byte, error) {
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add(authkey, authval)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
